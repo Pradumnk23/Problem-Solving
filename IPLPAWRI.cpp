40d1bed589be0e78deb7ff9f5a1e05f455562eb4
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-14 14:38:42 +0530

    Use map, 1st for number and 2nd for count

--- IPLPAWRI.cpp
+++ IPLPAWRI.cpp
@@ -1,34 +1,56 @@
-#include <iostream>
-#include<iterator>
-#include<map>
+#include <bits/stdc++.h>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+#define lli long long int
 using namespace std;
 
-int main() {
-	// your code goes here
-
-	int t;
-	long n;
-	cin>>t;
-	for(int i=0;i<t;i++)
-	{
-	    cin>>n;
-	    long a;
-	    map<long,long> b;
-	    
-	    for(int j=0;j<n;j++)
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+int main()
+{
+    fastio;
+    test()
+    {
+        lli n;
+        cin>>n;
+        lli a[n];
+        fn cin>>a[i];
+        unordered_map<lli, lli> mp;
+        for (int i = 0; i < n; i++)
+            mp[a[i]]++;
+        for(auto it:mp)
 	    {
-	       cin >> a;
-	        b[a]+=1;
-	    }
-	    for(auto x:b)
-	    {
-	        if(x.second == 3)
+	        if(it.second==3)
 	        {
-	            cout << (x.first * 3 ) << endl;
+	            cout<<it.first*3<<endl;
+	            break;
 	        }
 	    }
-	   
-	}
-	
-	return 0;
+    }
+    return 0;
 }
