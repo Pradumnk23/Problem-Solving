de90c5a4528dba7f43c39555e46feb54ae6c8917
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-16 01:47:53 +0530

    Create Save water save life.cpp

---
+++ Save water save life.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+
+int gcd(int a, int b)
+{
+    if(a==0) return 0;
+    else return gcd(b%a, a) + b/a;
+}
+
+int main()
+{
+    fastio;
+    test()
+    {
+        lli h,x,y,c;
+        cin>>h>>x>>y>>c;
+        lli ans=x+y/2;
+        if(ans*h<=c)
+            yes;
+        else
+            no;
+    }
+    return 0;
+}
